1823b4f8c16f9a47ed01db108ce245156dd899f3
Dmitry Shapovalov <shenf@mail.ru>
2018-12-11 23:44:11 +0300

    Собственный поток для вывода

---
+++ src/main/java/ru/d_shap/cli/io/OutputStreamWrapper.java
@@ -0,0 +1,100 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Command line interface provides facilities for rapid application interface prototyping.
+// Copyright (C) 2019 Dmitry Shapovalov.
+//
+// This file is part of command line interface.
+//
+// Command line interface is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Command line interface is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.cli.io;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Output stream wrapper.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class OutputStreamWrapper extends OutputStream {
+
+    private final OutputStream _outputStream;
+
+    private final OutputStream _logOutputStream;
+
+    public OutputStreamWrapper(final OutputStream outputStream) {
+        this(outputStream, null);
+    }
+
+    public OutputStreamWrapper(final OutputStream outputStream, final OutputStream logOutputStream) {
+        super();
+        _outputStream = outputStream;
+        _logOutputStream = logOutputStream;
+    }
+
+    @Override
+    public void write(final int value) throws IOException {
+        _outputStream.write(value);
+        if (_logOutputStream != null) {
+            _logOutputStream.write(value);
+        }
+    }
+
+    @Override
+    public void write(final byte[] value) throws IOException {
+        _outputStream.write(value);
+        if (_logOutputStream != null) {
+            _logOutputStream.write(value);
+        }
+    }
+
+    @Override
+    public void write(final byte[] value, final int offset, final int length) throws IOException {
+        _outputStream.write(value, offset, length);
+        if (_logOutputStream != null) {
+            _logOutputStream.write(value, offset, length);
+        }
+    }
+
+    @Override
+    public void flush() throws IOException {
+        _outputStream.flush();
+        if (_logOutputStream != null) {
+            _logOutputStream.flush();
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        IOException exception = null;
+        try {
+            _outputStream.close();
+        } catch (IOException ex) {
+            exception = ex;
+        }
+        if (_logOutputStream != null) {
+            try {
+                _logOutputStream.close();
+            } catch (IOException ex) {
+                if (exception == null) {
+                    exception = ex;
+                }
+            }
+        }
+        if (exception != null) {
+            throw exception;
+        }
+    }
+
+}
