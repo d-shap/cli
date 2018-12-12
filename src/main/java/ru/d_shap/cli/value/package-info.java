3ec7d94e36636a7237422050837963d19993b18b
Dmitry Shapovalov <shenf@mail.ru>
2018-12-12 15:59:31 +0300

    Добавил javadoc для пакета

---
+++ src/main/java/ru/d_shap/cli/value/package-info.java
@@ -0,0 +1,23 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Command line interface tools provide facilities for application interface development.
+// Copyright (C) 2019 Dmitry Shapovalov.
+//
+// This file is part of command line interface.
+//
+// Command line interface is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Command line interface is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * Command data classes.
+ */
+package ru.d_shap.cli.value;
