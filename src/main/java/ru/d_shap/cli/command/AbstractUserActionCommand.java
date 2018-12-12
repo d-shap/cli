4508c5014d0c1e846ff29789d4816efa346263ae
Dmitry Shapovalov <shenf@mail.ru>
2018-12-12 22:46:32 +0300

    Добавил заголовок файла и javadoc

--- src/main/java/ru/d_shap/cli/command/AbstractUserActionCommand.java
+++ src/main/java/ru/d_shap/cli/command/AbstractUserActionCommand.java
@@ -1,3 +1,22 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// CLI tools provide facilities for the command line interface development.
+// Copyright (C) 2019 Dmitry Shapovalov.
+//
+// This file is part of CLI tools.
+//
+// CLI tools is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CLI tools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
 package ru.d_shap.cli.command;
 
 import java.io.BufferedReader;
@@ -41,8 +60,21 @@ public abstract class AbstractUserActionCommand extends AbstractCommand {
         }
     }
 
+    /**
+     * Print the message.
+     *
+     * @param writer stream to write the command output.
+     */
     protected abstract void printMessage(PrintWriter writer);
 
+    /**
+     * Process the user input.
+     *
+     * @param input  the user input.
+     * @param writer stream to write the command output.
+     *
+     * @return the next command to execute.
+     */
     protected abstract Command processInput(String input, PrintWriter writer);
 
 }
