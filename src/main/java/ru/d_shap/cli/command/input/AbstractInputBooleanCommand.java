2fc57743c79518f3541d0c66e9824a210990120d
Dmitry Shapovalov <shenf@mail.ru>
2018-12-13 15:09:56 +0300

    Базовый класс для ввода значений пользователя

---
+++ src/main/java/ru/d_shap/cli/command/input/AbstractInputBooleanCommand.java
@@ -0,0 +1,135 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// CLI tools provide facilities for the command line interface development.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of CLI tools.
+//
+// CLI tools is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CLI tools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.cli.command.input;
+
+import java.util.Set;
+
+import ru.d_shap.cli.Command;
+import ru.d_shap.cli.data.ValueHolder;
+import ru.d_shap.cli.data.ValueLoader;
+
+/**
+ * Base class for all Boolean input commands.
+ *
+ * @author Dmitry Shapovalov
+ */
+public abstract class AbstractInputBooleanCommand extends AbstractInputCommand<Boolean> {
+
+    private final ValueHolder<Set<String>> _trueValues;
+
+    private final ValueHolder<Set<String>> _falseValues;
+
+    /**
+     * Create new object.
+     */
+    protected AbstractInputBooleanCommand() {
+        this(null);
+    }
+
+    /**
+     * Create new object.
+     *
+     * @param parentCommand the parent command.
+     */
+    protected AbstractInputBooleanCommand(final Command parentCommand) {
+        super(parentCommand);
+        _trueValues = new ValueHolder<>(new TrueValuesLoader());
+        _falseValues = new ValueHolder<>(new FalseValuesLoader());
+    }
+
+    /**
+     * Get the valid true values.
+     *
+     * @return the valid true values.
+     */
+    protected abstract Set<String> getTrueValues();
+
+    /**
+     * Get the valid false values.
+     *
+     * @return the valid false values.
+     */
+    protected abstract Set<String> getFalseValues();
+
+    @Override
+    protected final boolean isValidType(final String input) {
+        Set<String> trueValues = _trueValues.getValue();
+        for (String trueValue : trueValues) {
+            if (trueValue.equalsIgnoreCase(input)) {
+                return true;
+            }
+        }
+        Set<String> falseValues = _falseValues.getValue();
+        for (String falseValue : falseValues) {
+            if (falseValue.equalsIgnoreCase(input)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected final Boolean getValue(final String input) {
+        Set<String> trueValues = _trueValues.getValue();
+        for (String trueValue : trueValues) {
+            if (trueValue.equalsIgnoreCase(input)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Value loader for the true values.
+     *
+     * @author Dmitry Shapovalov
+     */
+    private final class TrueValuesLoader implements ValueLoader<Set<String>> {
+
+        TrueValuesLoader() {
+            super();
+        }
+
+        @Override
+        public Set<String> loadValue() {
+            return getTrueValues();
+        }
+
+    }
+
+    /**
+     * Value loader for the false values.
+     *
+     * @author Dmitry Shapovalov
+     */
+    private final class FalseValuesLoader implements ValueLoader<Set<String>> {
+
+        FalseValuesLoader() {
+            super();
+        }
+
+        @Override
+        public Set<String> loadValue() {
+            return getFalseValues();
+        }
+
+    }
+
+}
