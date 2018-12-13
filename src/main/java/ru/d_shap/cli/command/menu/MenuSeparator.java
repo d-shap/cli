b1e8176deaf16f0a92cf60782bcdffd997e614c2
Dmitry Shapovalov <shenf@mail.ru>
2018-12-13 10:38:57 +0300

    Разделитель элементов меню

---
+++ src/main/java/ru/d_shap/cli/command/menu/MenuSeparator.java
@@ -0,0 +1,55 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// CLI tools provide facilities for the command line interface development.
+// Copyright (C) 2019 Dmitry Shapovalov.
+//
+// This file is part of CLI tools.
+//
+// CLI tools is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CLI tools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.cli.command.menu;
+
+import java.io.PrintWriter;
+
+import ru.d_shap.cli.Command;
+
+/**
+ * Menu separator.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class MenuSeparator implements Option {
+
+    /**
+     * Create new object.
+     */
+    public MenuSeparator() {
+        super();
+    }
+
+    @Override
+    public void print(final PrintWriter writer, final boolean isDefault) {
+        writer.println();
+    }
+
+    @Override
+    public boolean isSelected(final char symbol) {
+        return false;
+    }
+
+    @Override
+    public Command getCommand() {
+        return null;
+    }
+
+}
