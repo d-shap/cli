ab6d1074cfc8fd1460334c03f2bf97c0bbde9360
Dmitry Shapovalov <shenf@mail.ru>
2018-12-13 15:25:11 +0300

    Базовый класс команды-контейнера

---
+++ src/main/java/ru/d_shap/cli/command/AbstractContainerCommand.java
@@ -0,0 +1,106 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// CLI tools provide facilities for the command line interface development.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of CLI tools.
+//
+// CLI tools is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CLI tools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.cli.command;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
+import ru.d_shap.cli.Command;
+import ru.d_shap.cli.data.Context;
+import ru.d_shap.cli.data.ValueHolder;
+import ru.d_shap.cli.data.ValueLoader;
+
+/**
+ * Base class for all commands that run contained commands within its own context.
+ *
+ * @author Dmitry Shapovalov
+ */
+public abstract class AbstractContainerCommand extends AbstractCommand {
+
+    private final ValueHolder<Command> _startCommand;
+
+    /**
+     * Create new object.
+     */
+    protected AbstractContainerCommand() {
+        this(null);
+    }
+
+    /**
+     * Create new object.
+     *
+     * @param parentCommand the parent command.
+     */
+    protected AbstractContainerCommand(final Command parentCommand) {
+        super(parentCommand);
+        _startCommand = new ValueHolder<>(new StartCommandLoader());
+    }
+
+    /**
+     * Get the command to execute within container.
+     *
+     * @return the command to execute within container.
+     */
+    protected abstract Command getStartCommand();
+
+    @Override
+    public final Command execute(final PrintWriter writer, final BufferedReader reader) {
+        Context context = new Context(getContext());
+        initContext(context);
+        Command startCommand = _startCommand.getValue();
+        getCommandRunner().execute(startCommand, context);
+        return processContext(context);
+    }
+
+    /**
+     * Initialize the context.
+     *
+     * @param context the context.
+     */
+    protected abstract void initContext(Context context);
+
+    /**
+     * Process the result context after the contained commands executed.
+     *
+     * @param context the result context.
+     *
+     * @return the next command to execute.
+     */
+    protected abstract Command processContext(Context context);
+
+    /**
+     * Value loader for the start command.
+     *
+     * @author Dmitry Shapovalov
+     */
+    private final class StartCommandLoader implements ValueLoader<Command> {
+
+        StartCommandLoader() {
+            super();
+        }
+
+        @Override
+        public Command loadValue() {
+            return getStartCommand();
+        }
+
+    }
+
+}
