c19097845b87b86d96e3b148b6bff1e2eb8aaaff
Dmitry Shapovalov <shenf@mail.ru>
2018-12-13 10:52:19 +0300

    Сделал возможность выбирать пункты меню не одним символом

--- src/main/java/ru/d_shap/cli/command/menu/MenuSeparator.java
+++ src/main/java/ru/d_shap/cli/command/menu/MenuSeparator.java
@@ -43,7 +43,7 @@ public final class MenuSeparator implements Option {
     }
 
     @Override
-    public boolean isSelected(final char symbol) {
+    public boolean isSelected(final String symbol) {
         return false;
     }
 
