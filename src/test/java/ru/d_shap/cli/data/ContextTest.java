db28592c0f5dc8cfdd0f0bd728c693e07c11d859
Dmitry Shapovalov <shenf@mail.ru>
2018-12-24 14:44:02 +0300

    Реализовал тесты

--- src/test/java/ru/d_shap/cli/data/ContextTest.java
+++ src/test/java/ru/d_shap/cli/data/ContextTest.java
@@ -21,6 +21,7 @@ package ru.d_shap.cli.data;
 
 import org.junit.Test;
 
+import ru.d_shap.assertions.Assertions;
 import ru.d_shap.cli.BaseCliTest;
 
 /**
@@ -42,7 +43,44 @@ public final class ContextTest extends BaseCliTest {
      */
     @Test
     public void getNamesTest() {
+        Context context11 = new Context();
+        Assertions.assertThat(context11.getNames()).containsExactlyInOrder();
+        Context context12 = new Context(null);
+        Assertions.assertThat(context12.getNames()).containsExactlyInOrder();
+        Context context13 = new Context(context11);
+        Assertions.assertThat(context13.getNames()).containsExactlyInOrder();
 
+        Context context21 = new Context();
+        context21.putValue("name1", "value1");
+        Assertions.assertThat(context21.getNames()).containsExactlyInOrder("name1");
+        Context context22 = new Context(null);
+        context22.putValue("name1", "value1");
+        Assertions.assertThat(context22.getNames()).containsExactlyInOrder("name1");
+        Context context23 = new Context(context21);
+        Assertions.assertThat(context23.getNames()).containsExactlyInOrder("name1");
+        context23.putValue("name2", "value2");
+        Assertions.assertThat(context23.getNames()).containsExactlyInOrder("name1", "name2");
+        context23.removeValue("name1");
+        Assertions.assertThat(context23.getNames()).containsExactlyInOrder("name2");
+
+        Context context31 = new Context();
+        context31.putValue("name2", "value2");
+        context31.putValue("name1", "value1");
+        context31.putValue("name3", "value3");
+        Assertions.assertThat(context31.getNames()).containsExactlyInOrder("name2", "name1", "name3");
+        Context context32 = new Context(null);
+        context32.putValue("name2", "value2");
+        context32.putValue("name1", "value1");
+        context32.putValue("name3", "value3");
+        Assertions.assertThat(context32.getNames()).containsExactlyInOrder("name2", "name1", "name3");
+        Context context33 = new Context(context31);
+        context33.putValue("name1", "value1");
+        context33.putValue("name2", "value2");
+        context33.putValue("name4", "value4");
+        Assertions.assertThat(context33.getNames()).containsExactlyInOrder("name2", "name1", "name3", "name4");
+        context33.removeValue("name3");
+        context33.putValue("name3", "value3");
+        Assertions.assertThat(context33.getNames()).containsExactlyInOrder("name2", "name1", "name4", "name3");
     }
 
     /**
@@ -50,7 +88,116 @@ public final class ContextTest extends BaseCliTest {
      */
     @Test
     public void hasValueTest() {
+        Context context1 = new Context();
+        Assertions.assertThat(context1.hasValue(null)).isFalse();
+        Assertions.assertThat(context1.hasValue("")).isFalse();
+        Assertions.assertThat(context1.hasValue(" ")).isFalse();
+        Assertions.assertThat(context1.hasValue("name1")).isFalse();
+        Assertions.assertThat(context1.hasValue("name2")).isFalse();
+        context1.putValue(null, "value");
+        Assertions.assertThat(context1.hasValue(null)).isFalse();
+        Assertions.assertThat(context1.hasValue("")).isFalse();
+        Assertions.assertThat(context1.hasValue(" ")).isFalse();
+        Assertions.assertThat(context1.hasValue("name1")).isFalse();
+        Assertions.assertThat(context1.hasValue("name2")).isFalse();
+        context1.putValue("", "value");
+        Assertions.assertThat(context1.hasValue(null)).isFalse();
+        Assertions.assertThat(context1.hasValue("")).isTrue();
+        Assertions.assertThat(context1.hasValue(" ")).isFalse();
+        Assertions.assertThat(context1.hasValue("name1")).isFalse();
+        Assertions.assertThat(context1.hasValue("name2")).isFalse();
+        context1.putValue("name1", "value1");
+        Assertions.assertThat(context1.hasValue(null)).isFalse();
+        Assertions.assertThat(context1.hasValue("")).isTrue();
+        Assertions.assertThat(context1.hasValue(" ")).isFalse();
+        Assertions.assertThat(context1.hasValue("name1")).isTrue();
+        Assertions.assertThat(context1.hasValue("name2")).isFalse();
+        context1.putValue("name2", "value2");
+        Assertions.assertThat(context1.hasValue(null)).isFalse();
+        Assertions.assertThat(context1.hasValue("")).isTrue();
+        Assertions.assertThat(context1.hasValue(" ")).isFalse();
+        Assertions.assertThat(context1.hasValue("name1")).isTrue();
+        Assertions.assertThat(context1.hasValue("name2")).isTrue();
+        context1.removeValue("name1");
+        Assertions.assertThat(context1.hasValue(null)).isFalse();
+        Assertions.assertThat(context1.hasValue("")).isTrue();
+        Assertions.assertThat(context1.hasValue(" ")).isFalse();
+        Assertions.assertThat(context1.hasValue("name1")).isFalse();
+        Assertions.assertThat(context1.hasValue("name2")).isTrue();
 
+        Context context2 = new Context(null);
+        Assertions.assertThat(context2.hasValue(null)).isFalse();
+        Assertions.assertThat(context2.hasValue("")).isFalse();
+        Assertions.assertThat(context2.hasValue(" ")).isFalse();
+        Assertions.assertThat(context2.hasValue("name1")).isFalse();
+        Assertions.assertThat(context2.hasValue("name2")).isFalse();
+        context2.putValue(null, "value");
+        Assertions.assertThat(context2.hasValue(null)).isFalse();
+        Assertions.assertThat(context2.hasValue("")).isFalse();
+        Assertions.assertThat(context2.hasValue(" ")).isFalse();
+        Assertions.assertThat(context2.hasValue("name1")).isFalse();
+        Assertions.assertThat(context2.hasValue("name2")).isFalse();
+        context2.putValue("", "value");
+        Assertions.assertThat(context2.hasValue(null)).isFalse();
+        Assertions.assertThat(context2.hasValue("")).isTrue();
+        Assertions.assertThat(context2.hasValue(" ")).isFalse();
+        Assertions.assertThat(context2.hasValue("name1")).isFalse();
+        Assertions.assertThat(context2.hasValue("name2")).isFalse();
+        context2.putValue("name1", "value1");
+        Assertions.assertThat(context2.hasValue(null)).isFalse();
+        Assertions.assertThat(context2.hasValue("")).isTrue();
+        Assertions.assertThat(context2.hasValue(" ")).isFalse();
+        Assertions.assertThat(context2.hasValue("name1")).isTrue();
+        Assertions.assertThat(context2.hasValue("name2")).isFalse();
+        context2.putValue("name2", "value2");
+        Assertions.assertThat(context2.hasValue(null)).isFalse();
+        Assertions.assertThat(context2.hasValue("")).isTrue();
+        Assertions.assertThat(context2.hasValue(" ")).isFalse();
+        Assertions.assertThat(context2.hasValue("name1")).isTrue();
+        Assertions.assertThat(context2.hasValue("name2")).isTrue();
+        context2.removeValue("name1");
+        Assertions.assertThat(context2.hasValue(null)).isFalse();
+        Assertions.assertThat(context2.hasValue("")).isTrue();
+        Assertions.assertThat(context2.hasValue(" ")).isFalse();
+        Assertions.assertThat(context2.hasValue("name1")).isFalse();
+        Assertions.assertThat(context2.hasValue("name2")).isTrue();
+
+        Context context3 = new Context(context1);
+        Assertions.assertThat(context3.hasValue(null)).isFalse();
+        Assertions.assertThat(context3.hasValue("")).isTrue();
+        Assertions.assertThat(context3.hasValue(" ")).isFalse();
+        Assertions.assertThat(context3.hasValue("name1")).isFalse();
+        Assertions.assertThat(context3.hasValue("name2")).isTrue();
+        context3.putValue(null, "value");
+        Assertions.assertThat(context3.hasValue(null)).isFalse();
+        Assertions.assertThat(context3.hasValue("")).isTrue();
+        Assertions.assertThat(context3.hasValue(" ")).isFalse();
+        Assertions.assertThat(context3.hasValue("name1")).isFalse();
+        Assertions.assertThat(context3.hasValue("name2")).isTrue();
+        context3.putValue("", "value");
+        Assertions.assertThat(context3.hasValue(null)).isFalse();
+        Assertions.assertThat(context3.hasValue("")).isTrue();
+        Assertions.assertThat(context3.hasValue(" ")).isFalse();
+        Assertions.assertThat(context3.hasValue("name1")).isFalse();
+        Assertions.assertThat(context3.hasValue("name2")).isTrue();
+        context3.putValue("name1", "value1");
+        Assertions.assertThat(context3.hasValue(null)).isFalse();
+        Assertions.assertThat(context3.hasValue("")).isTrue();
+        Assertions.assertThat(context3.hasValue(" ")).isFalse();
+        Assertions.assertThat(context3.hasValue("name1")).isTrue();
+        Assertions.assertThat(context3.hasValue("name2")).isTrue();
+        context3.putValue("name2", "value2");
+        Assertions.assertThat(context3.hasValue(null)).isFalse();
+        Assertions.assertThat(context3.hasValue("")).isTrue();
+        Assertions.assertThat(context3.hasValue(" ")).isFalse();
+        Assertions.assertThat(context3.hasValue("name1")).isTrue();
+        Assertions.assertThat(context3.hasValue("name2")).isTrue();
+        context3.removeValue("name1");
+        Assertions.assertThat(context3.hasValue(null)).isFalse();
+        Assertions.assertThat(context3.hasValue("")).isTrue();
+        Assertions.assertThat(context3.hasValue(" ")).isFalse();
+        Assertions.assertThat(context3.hasValue("name1")).isFalse();
+        Assertions.assertThat(context3.hasValue("name2")).isTrue();
     }
 
     /**
