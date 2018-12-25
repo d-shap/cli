7a74cf7c856b0e0542ad93fc01df90d01cd477c2
Dmitry Shapovalov <shenf@mail.ru>
2018-12-25 17:02:20 +0300

    Реализовал заглушки для тестов

---
+++ src/test/java/ru/d_shap/cli/command/menu/MenuItemTest.java
@@ -0,0 +1,80 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// CLI tools provide facilities for the command line interface development.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of CLI tools.
+//
+// CLI tools is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CLI tools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.cli.command.menu;
+
+import org.junit.Test;
+
+import ru.d_shap.cli.BaseCliTest;
+
+/**
+ * Tests for {@link MenuItem}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class MenuItemTest extends BaseCliTest {
+
+    /**
+     * Test class constructor.
+     */
+    public MenuItemTest() {
+        super();
+    }
+
+    /**
+     * {@link MenuItem} class test.
+     */
+    @Test
+    public void getSymbolTest() {
+
+    }
+
+    /**
+     * {@link MenuItem} class test.
+     */
+    @Test
+    public void getLabelTest() {
+
+    }
+
+    /**
+     * {@link MenuItem} class test.
+     */
+    @Test
+    public void printTest() {
+
+    }
+
+    /**
+     * {@link MenuItem} class test.
+     */
+    @Test
+    public void isSelectedTest() {
+
+    }
+
+    /**
+     * {@link MenuItem} class test.
+     */
+    @Test
+    public void getCommandTest() {
+
+    }
+
+}
