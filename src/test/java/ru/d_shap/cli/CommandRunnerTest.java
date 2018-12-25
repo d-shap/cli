03b3b7510c3fba4ecb5310ccc211e186b9c73b6b
Dmitry Shapovalov <shenf@mail.ru>
2018-12-25 13:10:37 +0300

    Добавил тесты на Reader

--- src/test/java/ru/d_shap/cli/CommandRunnerTest.java
+++ src/test/java/ru/d_shap/cli/CommandRunnerTest.java
@@ -19,6 +19,7 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 package ru.d_shap.cli;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintWriter;
@@ -137,7 +138,69 @@ public final class CommandRunnerTest extends BaseCliTest {
      */
     @Test
     public void getReaderTest() {
+        ByteArrayOutputStream os1 = createOutputStream();
+        InputStream is1 = createInputStream("Строка 1", "Строка 2");
+        CommandRunner commandRunner1 = new CommandRunner(os1, is1);
+        BufferedReader reader1 = commandRunner1.getReader();
+        Assertions.assertThat(getLines(reader1)).containsExactlyInOrder("Строка 1", "Строка 2");
 
+        ByteArrayOutputStream os21 = createOutputStream();
+        ByteArrayOutputStream os22 = createOutputStream();
+        InputStream is2 = createInputStream("Строка 1", "Строка 2");
+        CommandRunner commandRunner2 = new CommandRunner(os21, is2, os22);
+        BufferedReader reader2 = commandRunner2.getReader();
+        Assertions.assertThat(getLines(reader2)).containsExactlyInOrder("Строка 1", "Строка 2");
+        commandRunner2.getWriter().flush();
+        Assertions.assertThat(getLines(os22)).containsExactlyInOrder("Строка 1", "Строка 2");
+
+        ByteArrayOutputStream os3 = createOutputStream();
+        InputStream is3 = createInputStreamWithCharset(CHARSET_NAME, "Строка 1", "Строка 2");
+        CommandRunner commandRunner3 = new CommandRunner(os3, is3, CHARSET_NAME);
+        BufferedReader reader3 = commandRunner3.getReader();
+        Assertions.assertThat(getLines(reader3)).containsExactlyInOrder("Строка 1", "Строка 2");
+
+        ByteArrayOutputStream os41 = createOutputStream();
+        ByteArrayOutputStream os42 = createOutputStream();
+        InputStream is4 = createInputStreamWithCharset(CHARSET_NAME, "Строка 1", "Строка 2");
+        CommandRunner commandRunner4 = new CommandRunner(os41, is4, os42, CHARSET_NAME);
+        BufferedReader reader4 = commandRunner4.getReader();
+        Assertions.assertThat(getLines(reader4)).containsExactlyInOrder("Строка 1", "Строка 2");
+        commandRunner4.getWriter().flush();
+        Assertions.assertThat(getLines(CHARSET_NAME, os42)).containsExactlyInOrder("Строка 1", "Строка 2");
+
+        ByteArrayOutputStream os5 = createOutputStream();
+        InputStream is5 = createInputStreamWithCharset(CHARSET_NAME, "Строка 1", "Строка 2");
+        CommandRunner commandRunner5 = new CommandRunner(os5, is5, Charset.forName(CHARSET_NAME));
+        BufferedReader reader5 = commandRunner5.getReader();
+        Assertions.assertThat(getLines(reader5)).containsExactlyInOrder("Строка 1", "Строка 2");
+
+        ByteArrayOutputStream os61 = createOutputStream();
+        ByteArrayOutputStream os62 = createOutputStream();
+        InputStream is6 = createInputStreamWithCharset(CHARSET_NAME, "Строка 1", "Строка 2");
+        CommandRunner commandRunner6 = new CommandRunner(os61, is6, os62, Charset.forName(CHARSET_NAME));
+        BufferedReader reader6 = commandRunner6.getReader();
+        Assertions.assertThat(getLines(reader6)).containsExactlyInOrder("Строка 1", "Строка 2");
+        commandRunner6.getWriter().flush();
+        Assertions.assertThat(getLines(CHARSET_NAME, os62)).containsExactlyInOrder("Строка 1", "Строка 2");
+
+        try {
+            ByteArrayOutputStream os7 = createOutputStream();
+            InputStream is7 = createInputStream("Строка 1", "Строка 2");
+            new CommandRunner(os7, is7, WRONG_CHARSET_NAME);
+            Assertions.fail("CommandRunner test fail");
+        } catch (CliIOException ex) {
+            Assertions.assertThat(ex).hasCause(UnsupportedEncodingException.class);
+        }
+
+        try {
+            ByteArrayOutputStream os81 = createOutputStream();
+            ByteArrayOutputStream os82 = createOutputStream();
+            InputStream is8 = createInputStream("Строка 1", "Строка 2");
+            new CommandRunner(os81, is8, os82, WRONG_CHARSET_NAME);
+            Assertions.fail("CommandRunner test fail");
+        } catch (CliIOException ex) {
+            Assertions.assertThat(ex).hasCause(UnsupportedEncodingException.class);
+        }
     }
 
     /**
