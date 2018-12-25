a133a099ecfe22d30569b5c23cfd041ce08935ee
Dmitry Shapovalov <shenf@mail.ru>
2018-12-25 15:30:38 +0300

    Добавил вывод сообщения в выходной поток

--- src/test/java/ru/d_shap/cli/command/AbstractUserActionCommandImpl.java
+++ src/test/java/ru/d_shap/cli/command/AbstractUserActionCommandImpl.java
@@ -55,7 +55,7 @@ public final class AbstractUserActionCommandImpl extends AbstractUserActionComma
 
     @Override
     protected void printMessage(final PrintWriter writer) {
-        writer.println();
+        writer.println(_message);
     }
 
     @Override
