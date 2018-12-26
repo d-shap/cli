09410290029d9de47e557375ea9d921b05ede5af
Dmitry Shapovalov <shenf@mail.ru>
2018-12-26 16:41:01 +0300

    Реализовал тесты

--- src/test/java/ru/d_shap/cli/command/menu/AbstractYesNoCommandTest.java
+++ src/test/java/ru/d_shap/cli/command/menu/AbstractYesNoCommandTest.java
@@ -19,9 +19,18 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 package ru.d_shap.cli.command.menu;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
 import org.junit.Test;
 
+import ru.d_shap.assertions.Assertions;
 import ru.d_shap.cli.BaseCliTest;
+import ru.d_shap.cli.Command;
+import ru.d_shap.cli.CommandRunner;
+import ru.d_shap.cli.command.AbstractExecutionCommandImpl;
+import ru.d_shap.cli.command.CommandDefinitionException;
+import ru.d_shap.cli.data.Lines;
 
 /**
  * Tests for {@link AbstractYesNoCommand}.
@@ -42,7 +51,62 @@ public final class AbstractYesNoCommandTest extends BaseCliTest {
      */
     @Test
     public void getYesMenuItemSymbolTest() {
+        ByteArrayOutputStream os1 = createOutputStream();
+        InputStream is1 = createInputStream("y");
+        CommandRunner commandRunner1 = new CommandRunner(os1, is1);
+        Command childCommand11 = new AbstractExecutionCommandImpl("yes selected");
+        Command childCommand12 = new AbstractExecutionCommandImpl("no selected");
+        AbstractYesNoCommandImpl command1 = new AbstractYesNoCommandImpl(new Lines("line"), "y", new Lines("Yes"), childCommand11, "n", new Lines("No"), childCommand12, AbstractMenuCommand.DEFAULT_SYMBOL_LENGTH, AbstractMenuCommand.NO_DEFAULT_OPTION_INDEX, "wrong: <%s>");
+        commandRunner1.execute(command1);
+        Assertions.assertThat(getLines(os1)).containsExactlyInOrder("line", "      y: Yes", "      n: No", "", "yes selected");
 
+        ByteArrayOutputStream os2 = createOutputStream();
+        InputStream is2 = createInputStream("yes");
+        CommandRunner commandRunner2 = new CommandRunner(os2, is2);
+        Command childCommand21 = new AbstractExecutionCommandImpl("yes selected");
+        Command childCommand22 = new AbstractExecutionCommandImpl("no selected");
+        AbstractYesNoCommandImpl command2 = new AbstractYesNoCommandImpl(new Lines("line"), "yes", new Lines("Yes"), childCommand21, "no", new Lines("No"), childCommand22, AbstractMenuCommand.DEFAULT_SYMBOL_LENGTH, AbstractMenuCommand.NO_DEFAULT_OPTION_INDEX, "wrong: <%s>");
+        commandRunner2.execute(command2);
+        Assertions.assertThat(getLines(os2)).containsExactlyInOrder("line", "    yes: Yes", "     no: No", "", "yes selected");
+
+        try {
+            ByteArrayOutputStream os = createOutputStream();
+            InputStream is = createInputStream("y");
+            CommandRunner commandRunner = new CommandRunner(os, is);
+            Command childCommand1 = new AbstractExecutionCommandImpl("yes selected");
+            Command childCommand2 = new AbstractExecutionCommandImpl("no selected");
+            AbstractYesNoCommandImpl command = new AbstractYesNoCommandImpl(new Lines("line"), null, new Lines("Yes"), childCommand1, "n", new Lines("No"), childCommand2, AbstractMenuCommand.DEFAULT_SYMBOL_LENGTH, AbstractMenuCommand.NO_DEFAULT_OPTION_INDEX, "wrong: <%s>");
+            commandRunner.execute(command);
+            Assertions.fail("AbstractYesNoCommand test fail");
+        } catch (CommandDefinitionException ex) {
+            Assertions.assertThat(ex).hasMessage("Option symbol is not defined");
+        }
+
+        try {
+            ByteArrayOutputStream os = createOutputStream();
+            InputStream is = createInputStream("y");
+            CommandRunner commandRunner = new CommandRunner(os, is);
+            Command childCommand1 = new AbstractExecutionCommandImpl("yes selected");
+            Command childCommand2 = new AbstractExecutionCommandImpl("no selected");
+            AbstractYesNoCommandImpl command = new AbstractYesNoCommandImpl(new Lines("line"), "", new Lines("Yes"), childCommand1, "n", new Lines("No"), childCommand2, AbstractMenuCommand.DEFAULT_SYMBOL_LENGTH, AbstractMenuCommand.NO_DEFAULT_OPTION_INDEX, "wrong: <%s>");
+            commandRunner.execute(command);
+            Assertions.fail("AbstractYesNoCommand test fail");
+        } catch (CommandDefinitionException ex) {
+            Assertions.assertThat(ex).hasMessage("Option symbol is not defined");
+        }
+
+        try {
+            ByteArrayOutputStream os = createOutputStream();
+            InputStream is = createInputStream("y");
+            CommandRunner commandRunner = new CommandRunner(os, is);
+            Command childCommand1 = new AbstractExecutionCommandImpl("yes selected");
+            Command childCommand2 = new AbstractExecutionCommandImpl("no selected");
+            AbstractYesNoCommandImpl command = new AbstractYesNoCommandImpl(new Lines("line"), " ", new Lines("Yes"), childCommand1, "n", new Lines("No"), childCommand2, AbstractMenuCommand.DEFAULT_SYMBOL_LENGTH, AbstractMenuCommand.NO_DEFAULT_OPTION_INDEX, "wrong: <%s>");
+            commandRunner.execute(command);
+            Assertions.fail("AbstractYesNoCommand test fail");
+        } catch (CommandDefinitionException ex) {
+            Assertions.assertThat(ex).hasMessage("Option symbol is wrong:  ");
+        }
     }
 
     /**
@@ -66,7 +130,62 @@ public final class AbstractYesNoCommandTest extends BaseCliTest {
      */
     @Test
     public void getNoMenuItemSymbolTest() {
+        ByteArrayOutputStream os1 = createOutputStream();
+        InputStream is1 = createInputStream("n");
+        CommandRunner commandRunner1 = new CommandRunner(os1, is1);
+        Command childCommand11 = new AbstractExecutionCommandImpl("yes selected");
+        Command childCommand12 = new AbstractExecutionCommandImpl("no selected");
+        AbstractYesNoCommandImpl command1 = new AbstractYesNoCommandImpl(new Lines("line"), "y", new Lines("Yes"), childCommand11, "n", new Lines("No"), childCommand12, AbstractMenuCommand.DEFAULT_SYMBOL_LENGTH, AbstractMenuCommand.NO_DEFAULT_OPTION_INDEX, "wrong: <%s>");
+        commandRunner1.execute(command1);
+        Assertions.assertThat(getLines(os1)).containsExactlyInOrder("line", "      y: Yes", "      n: No", "", "no selected");
 
+        ByteArrayOutputStream os2 = createOutputStream();
+        InputStream is2 = createInputStream("no");
+        CommandRunner commandRunner2 = new CommandRunner(os2, is2);
+        Command childCommand21 = new AbstractExecutionCommandImpl("yes selected");
+        Command childCommand22 = new AbstractExecutionCommandImpl("no selected");
+        AbstractYesNoCommandImpl command2 = new AbstractYesNoCommandImpl(new Lines("line"), "yes", new Lines("Yes"), childCommand21, "no", new Lines("No"), childCommand22, AbstractMenuCommand.DEFAULT_SYMBOL_LENGTH, AbstractMenuCommand.NO_DEFAULT_OPTION_INDEX, "wrong: <%s>");
+        commandRunner2.execute(command2);
+        Assertions.assertThat(getLines(os2)).containsExactlyInOrder("line", "    yes: Yes", "     no: No", "", "no selected");
+
+        try {
+            ByteArrayOutputStream os = createOutputStream();
+            InputStream is = createInputStream("n");
+            CommandRunner commandRunner = new CommandRunner(os, is);
+            Command childCommand1 = new AbstractExecutionCommandImpl("yes selected");
+            Command childCommand2 = new AbstractExecutionCommandImpl("no selected");
+            AbstractYesNoCommandImpl command = new AbstractYesNoCommandImpl(new Lines("line"), "y", new Lines("Yes"), childCommand1, null, new Lines("No"), childCommand2, AbstractMenuCommand.DEFAULT_SYMBOL_LENGTH, AbstractMenuCommand.NO_DEFAULT_OPTION_INDEX, "wrong: <%s>");
+            commandRunner.execute(command);
+            Assertions.fail("AbstractYesNoCommand test fail");
+        } catch (CommandDefinitionException ex) {
+            Assertions.assertThat(ex).hasMessage("Option symbol is not defined");
+        }
+
+        try {
+            ByteArrayOutputStream os = createOutputStream();
+            InputStream is = createInputStream("n");
+            CommandRunner commandRunner = new CommandRunner(os, is);
+            Command childCommand1 = new AbstractExecutionCommandImpl("yes selected");
+            Command childCommand2 = new AbstractExecutionCommandImpl("no selected");
+            AbstractYesNoCommandImpl command = new AbstractYesNoCommandImpl(new Lines("line"), "y", new Lines("Yes"), childCommand1, "", new Lines("No"), childCommand2, AbstractMenuCommand.DEFAULT_SYMBOL_LENGTH, AbstractMenuCommand.NO_DEFAULT_OPTION_INDEX, "wrong: <%s>");
+            commandRunner.execute(command);
+            Assertions.fail("AbstractYesNoCommand test fail");
+        } catch (CommandDefinitionException ex) {
+            Assertions.assertThat(ex).hasMessage("Option symbol is not defined");
+        }
+
+        try {
+            ByteArrayOutputStream os = createOutputStream();
+            InputStream is = createInputStream("n");
+            CommandRunner commandRunner = new CommandRunner(os, is);
+            Command childCommand1 = new AbstractExecutionCommandImpl("yes selected");
+            Command childCommand2 = new AbstractExecutionCommandImpl("no selected");
+            AbstractYesNoCommandImpl command = new AbstractYesNoCommandImpl(new Lines("line"), "y", new Lines("Yes"), childCommand1, " ", new Lines("No"), childCommand2, AbstractMenuCommand.DEFAULT_SYMBOL_LENGTH, AbstractMenuCommand.NO_DEFAULT_OPTION_INDEX, "wrong: <%s>");
+            commandRunner.execute(command);
+            Assertions.fail("AbstractYesNoCommand test fail");
+        } catch (CommandDefinitionException ex) {
+            Assertions.assertThat(ex).hasMessage("Option symbol is wrong:  ");
+        }
     }
 
     /**
