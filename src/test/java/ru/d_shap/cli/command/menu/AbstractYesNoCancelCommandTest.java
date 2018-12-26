17aa311dc1d582cb36cb8cb9d14fd1eab345d8f1
Dmitry Shapovalov <shenf@mail.ru>
2018-12-26 15:15:36 +0300

    Заглушки для тестов

---
+++ src/test/java/ru/d_shap/cli/command/menu/AbstractYesNoCancelCommandTest.java
@@ -0,0 +1,112 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// CLI tools provide facilities for the command line interface development.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of CLI tools.
+//
+// CLI tools is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CLI tools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.cli.command.menu;
+
+import org.junit.Test;
+
+import ru.d_shap.cli.BaseCliTest;
+
+/**
+ * Tests for {@link AbstractYesNoCancelCommand}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class AbstractYesNoCancelCommandTest extends BaseCliTest {
+
+    /**
+     * Test class constructor.
+     */
+    public AbstractYesNoCancelCommandTest() {
+        super();
+    }
+
+    /**
+     * {@link AbstractYesNoCancelCommand} class test.
+     */
+    @Test
+    public void getYesMenuItemSymbolTest() {
+
+    }
+
+    /**
+     * {@link AbstractYesNoCancelCommand} class test.
+     */
+    @Test
+    public void getYesMenuItemLabelTest() {
+
+    }
+
+    /**
+     * {@link AbstractYesNoCancelCommand} class test.
+     */
+    @Test
+    public void getYesMenuItemCommandTest() {
+
+    }
+
+    /**
+     * {@link AbstractYesNoCancelCommand} class test.
+     */
+    @Test
+    public void getNoMenuItemSymbolTest() {
+
+    }
+
+    /**
+     * {@link AbstractYesNoCancelCommand} class test.
+     */
+    @Test
+    public void getNoMenuItemLabelTest() {
+
+    }
+
+    /**
+     * {@link AbstractYesNoCancelCommand} class test.
+     */
+    @Test
+    public void getNoMenuItemCommandTest() {
+
+    }
+
+    /**
+     * {@link AbstractYesNoCancelCommand} class test.
+     */
+    @Test
+    public void getCancelMenuItemSymbolTest() {
+
+    }
+
+    /**
+     * {@link AbstractYesNoCancelCommand} class test.
+     */
+    @Test
+    public void getCancelMenuItemLabelTest() {
+
+    }
+
+    /**
+     * {@link AbstractYesNoCancelCommand} class test.
+     */
+    @Test
+    public void executeTest() {
+
+    }
+
+}
---
+++ src/test/java/ru/d_shap/cli/command/menu/AbstractYesNoCommandTest.java
@@ -0,0 +1,96 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// CLI tools provide facilities for the command line interface development.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of CLI tools.
+//
+// CLI tools is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CLI tools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.cli.command.menu;
+
+import org.junit.Test;
+
+import ru.d_shap.cli.BaseCliTest;
+
+/**
+ * Tests for {@link AbstractYesNoCommand}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class AbstractYesNoCommandTest extends BaseCliTest {
+
+    /**
+     * Test class constructor.
+     */
+    public AbstractYesNoCommandTest() {
+        super();
+    }
+
+    /**
+     * {@link AbstractYesNoCommand} class test.
+     */
+    @Test
+    public void getYesMenuItemSymbolTest() {
+
+    }
+
+    /**
+     * {@link AbstractYesNoCommand} class test.
+     */
+    @Test
+    public void getYesMenuItemLabelTest() {
+
+    }
+
+    /**
+     * {@link AbstractYesNoCommand} class test.
+     */
+    @Test
+    public void getYesMenuItemCommandTest() {
+
+    }
+
+    /**
+     * {@link AbstractYesNoCommand} class test.
+     */
+    @Test
+    public void getNoMenuItemSymbolTest() {
+
+    }
+
+    /**
+     * {@link AbstractYesNoCommand} class test.
+     */
+    @Test
+    public void getNoMenuItemLabelTest() {
+
+    }
+
+    /**
+     * {@link AbstractYesNoCommand} class test.
+     */
+    @Test
+    public void getNoMenuItemCommandTest() {
+
+    }
+
+    /**
+     * {@link AbstractYesNoCommand} class test.
+     */
+    @Test
+    public void executeTest() {
+
+    }
+
+}
