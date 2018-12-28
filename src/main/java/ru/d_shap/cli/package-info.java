520b44c6f4c5b599ab18e0823e788905f3e66f1e
Dmitry Shapovalov <shenf@mail.ru>
2018-12-28 11:30:18 +0300

    Написал javadoc для библиотеки

--- src/main/java/ru/d_shap/cli/package-info.java
+++ src/main/java/ru/d_shap/cli/package-info.java
@@ -18,6 +18,47 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /**
- * Command line interface tools provide facilities for application interface development.
+ * <p>
+ * CLI tools provide facilities for the command line interface development.
+ * </p>
+ * <p>
+ * Command line interface can be defined as a command flow. The commands in this flow are executed
+ * step by step.
+ * </p>
+ * <p>
+ * The command implementation class should extend the {@link ru.d_shap.cli.command.AbstractCommand} class
+ * or it's subclasses. The {@link ru.d_shap.cli.command.AbstractCommand#execute(java.io.PrintWriter, java.io.BufferedReader)}
+ * method defines the next command to execute. So each command performs some actions and then defines
+ * the next command based on this actions. This is the command flow.
+ * </p>
+ * <p>
+ * The {@link ru.d_shap.cli.CommandRunner} class is used to run the command flow. This class obtains
+ * the first command in the flow and executes all the commands in this flow.
+ * </p>
+ * <p>
+ * Some commands require user interaction, some does not. For example, the application first collects
+ * the required data from the user and then performs some actions based on this data.
+ * </p>
+ * <p>
+ * Commands, that require the user interaction, should extend the {@link ru.d_shap.cli.command.AbstractUserActionCommand}
+ * class or it's subclasses. Some useful subclasses are {@link ru.d_shap.cli.command.menu.AbstractMenuCommand}
+ * to define the menu, {@link ru.d_shap.cli.command.input.AbstractInputCommand} to obtain the data
+ * from the user like strings, numbers etc.
+ * </p>
+ * <p>
+ * Commands, that does not require the user interaction, should extend the {@link ru.d_shap.cli.command.AbstractExecutionCommand}
+ * class or it's subclasses.
+ * </p>
+ * <p>
+ * All the commands in the flow share the same context. One command in the flow can put the value to
+ * the context, another command can use this value to perform some actions.
+ * </p>
+ * <p>
+ * The command flow can contain subflows. Subflow has the nested context. All the commands of the subflow
+ * share the same nested context, but subflow does not affect the parent context.
+ * </p>
+ * <p>
+ * The sublow is defined by the command class, that extends the {@link ru.d_shap.cli.command.AbstractContainerCommand}.
+ * </p>
  */
 package ru.d_shap.cli;
