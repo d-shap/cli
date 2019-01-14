1d19f54b9823201f11e35f52a2cb0690ad9615d2
Dmitry Shapovalov <shenf@mail.ru>
2019-01-14 16:00:47 +0300

    Добавил заглушки для тестов

---
+++ src/test/java/ru/d_shap/cli/command/CommandDelegateTest.java
@@ -0,0 +1,136 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// CLI tools provide facilities for the command line interface development.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of CLI tools.
+//
+// CLI tools is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// CLI tools is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.cli.command;
+
+import org.junit.Test;
+
+import ru.d_shap.cli.BaseCliTest;
+
+/**
+ * Tests for {@link CommandDelegate}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class CommandDelegateTest extends BaseCliTest {
+
+    /**
+     * Test class constructor.
+     */
+    public CommandDelegateTest() {
+        super();
+    }
+
+    /**
+     * {@link CommandDelegate} class test.
+     */
+    @Test
+    public void getCommandTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CommandDelegate} class test.
+     */
+    @Test
+    public void setCommandTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CommandDelegate} class test.
+     */
+    @Test
+    public void hasParentCommandTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CommandDelegate} class test.
+     */
+    @Test
+    public void hasParentCommandWithClassTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CommandDelegate} class test.
+     */
+    @Test
+    public void getParentCommandTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CommandDelegate} class test.
+     */
+    @Test
+    public void getParentCommandWithClassTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CommandDelegate} class test.
+     */
+    @Test
+    public void getContextTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CommandDelegate} class test.
+     */
+    @Test
+    public void setContextTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CommandDelegate} class test.
+     */
+    @Test
+    public void getCommandRunnerTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CommandDelegate} class test.
+     */
+    @Test
+    public void setCommandRunnerTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CommandDelegate} class test.
+     */
+    @Test
+    public void executeTest() {
+        // TODO
+    }
+
+    /**
+     * {@link CommandDelegate} class test.
+     */
+    @Test
+    public void resetTest() {
+        // TODO
+    }
+
+}
