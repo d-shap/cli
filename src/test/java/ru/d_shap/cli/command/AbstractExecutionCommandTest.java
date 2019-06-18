c855bef83fd93cfed68ffc290d3536f25d95f999
Dmitry Shapovalov <shenf@mail.ru>
2019-06-18 15:55:55 +0300

    Поправил тесты с учетом нового конструктора

--- src/test/java/ru/d_shap/cli/command/AbstractExecutionCommandTest.java
+++ src/test/java/ru/d_shap/cli/command/AbstractExecutionCommandTest.java
@@ -51,25 +51,43 @@ public final class AbstractExecutionCommandTest extends BaseCliTest {
         ByteArrayOutputStream os1 = createOutputStream();
         InputStream is1 = createInputStream();
         CommandRunner commandRunner1 = new CommandRunner(os1, is1);
-        Command command1 = new AbstractExecutionCommandImpl("Output 1");
+        Command command1 = new AbstractExecutionCommandImpl("Output 1", null);
         commandRunner1.execute(command1);
         Assertions.assertThat(getLines(os1)).containsExactlyInOrder("Output 1");
 
         ByteArrayOutputStream os2 = createOutputStream();
         InputStream is2 = createInputStream();
         CommandRunner commandRunner2 = new CommandRunner(os2, is2);
-        Command command2 = new AbstractExecutionCommandImpl("Output 1", null);
+        Command command2 = new AbstractExecutionCommandImpl(null, "Output 1", null);
         commandRunner2.execute(command2);
         Assertions.assertThat(getLines(os2)).containsExactlyInOrder("Output 1");
 
         ByteArrayOutputStream os3 = createOutputStream();
         InputStream is3 = createInputStream();
         CommandRunner commandRunner3 = new CommandRunner(os3, is3);
-        Command command33 = new AbstractExecutionCommandImpl("Output 3");
+        Command command33 = new AbstractExecutionCommandImpl("Output 3", null);
         Command command32 = new AbstractExecutionCommandImpl("Output 2", command33);
         Command command31 = new AbstractExecutionCommandImpl("Output 1", command32);
         commandRunner3.execute(command31);
         Assertions.assertThat(getLines(os3)).containsExactlyInOrder("Output 1", "Output 2", "Output 3");
+
+        ByteArrayOutputStream os4 = createOutputStream();
+        InputStream is4 = createInputStream();
+        CommandRunner commandRunner4 = new CommandRunner(os4, is4);
+        Command parentCommand4 = new AbstractExecutionCommandImpl(null, "parent", null);
+        Command command4 = new AbstractExecutionCommandImpl(parentCommand4, "Output 1", null);
+        commandRunner4.execute(command4);
+        Assertions.assertThat(getLines(os4)).containsExactlyInOrder("Output 1", "parent");
+
+        ByteArrayOutputStream os5 = createOutputStream();
+        InputStream is5 = createInputStream();
+        CommandRunner commandRunner5 = new CommandRunner(os5, is5);
+        Command parentCommand5 = new AbstractExecutionCommandImpl(null, "parent", null);
+        Command command53 = new AbstractExecutionCommandImpl("Output 3", null);
+        Command command52 = new AbstractExecutionCommandImpl("Output 2", command53);
+        Command command51 = new AbstractExecutionCommandImpl(parentCommand5, "Output 1", command52);
+        commandRunner5.execute(command51);
+        Assertions.assertThat(getLines(os5)).containsExactlyInOrder("Output 1", "Output 2", "Output 3");
     }
 
 }
